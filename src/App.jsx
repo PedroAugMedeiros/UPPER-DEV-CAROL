ff1816be7e1ecc47da17d46d2e702042be5f8037
PedroAugMedeiros <pedroaugmed7@gmail.com>
2024-10-23 19:09:30 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Routes from "./Routes";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes />
-    </Router>
+    </HashRouter>
   );
 }
 
