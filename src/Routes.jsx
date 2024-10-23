3acbb454847b1090e2f90904720a2dcfa819e20a
PedroAugMedeiros <pedroaugmed7@gmail.com>
2024-10-23 16:56:32 -0300

    fix

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -10,7 +10,7 @@ import MoveInAndMoveOut from "./pages/MoveInAndMoveOut/index";
 import TermsAndConditions from "./pages/TermsAndConditions/index";
 
 import RegularCleaning from "./pages/RegularCleaning/index";
-import AboutusPage from "./pages/Home";
+import AboutusPage from "./pages/Aboutus";
 
 const ProjectRoutes = () => {
   let element = useRoutes([
