85159d6ac3100047507363139b583444cd9f9c06
PedroAugMedeiros <pedroaugmed7@gmail.com>
2024-10-23 11:16:39 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,55 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { useRoutes } from "react-router-dom";
-// import NotFound from "pages/NotFound";
-import Home from "./pages/Home";
-import AboutUs from "./pages/Aboutus/index";
-import DeepCleaning from "./pages/Deepcleaning";
-import CommercialCleaning from "./pages/CommercialCleaning/index";
-import JoinUs from "./pages/JoinUs/index";
-import MoveInAndMoveOut from "./pages/MoveInAndMoveOut/index";
-import TermsAndConditions from "./pages/TermsAndConditions/index";
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import { useRoutes } from "react-router-dom";
+// // import NotFound from "pages/NotFound";
+// import Home from "./pages/Home";
+// import AboutUs from "./pages/Aboutus/index";
+// import DeepCleaning from "./pages/Deepcleaning";
+// import CommercialCleaning from "./pages/CommercialCleaning/index";
+// import JoinUs from "./pages/JoinUs/index";
+// import MoveInAndMoveOut from "./pages/MoveInAndMoveOut/index";
+// import TermsAndConditions from "./pages/TermsAndConditions/index";
 
-import RegularCleaning from "./pages/RegularCleaning/index";
-import AboutusPage from "./pages/Home";
+// import RegularCleaning from "./pages/RegularCleaning/index";
+// import AboutusPage from "./pages/Home";
 
-// const ProjectRoutes = () => {
-//   let element = useRoutes([
-//     { path: "/", element: <Home /> },
-//     { path: "/AboutUs", element: <AboutusPage /> },
-//     { path: "/DeepCleaning", element: <DeepCleaning /> },
-//     { path: "/CommercialCleaning", element: <CommercialCleaning /> },
-//     { path: "/JoinUs", element: <JoinUs /> },
-//     { path: "/MoveInAndMoveOut", element: <MoveInAndMoveOut /> },
-//     { path: "/RegularCleaning", element: <RegularCleaning /> },
-//     { path: "/TermsAndConditions", element: <TermsAndConditions /> },
-//     { path: "*", element: <NotFound /> },
-//     {
-//       path: "home",
-//       element: <Home />,
-//     },
-//     {
-//       path: "AboutUs",
-//       element: <Home />,
-//     },
-//   ]);
+// // const ProjectRoutes = () => {
+// //   let element = useRoutes([
+// //     { path: "/", element: <Home /> },
+// //     { path: "/AboutUs", element: <AboutusPage /> },
+// //     { path: "/DeepCleaning", element: <DeepCleaning /> },
+// //     { path: "/CommercialCleaning", element: <CommercialCleaning /> },
+// //     { path: "/JoinUs", element: <JoinUs /> },
+// //     { path: "/MoveInAndMoveOut", element: <MoveInAndMoveOut /> },
+// //     { path: "/RegularCleaning", element: <RegularCleaning /> },
+// //     { path: "/TermsAndConditions", element: <TermsAndConditions /> },
+// //     { path: "*", element: <NotFound /> },
+// //     {
+// //       path: "home",
+// //       element: <Home />,
+// //     },
+// //     {
+// //       path: "AboutUs",
+// //       element: <Home />,
+// //     },
+// //   ]);
 
-//   return element;
-// };
-function App() {
-  return (
-    <div className="root">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/Home" element={<Home />}></Route>
-        <Route path="./DeepCleaning" element={<DeepCleaning />}></Route>
-        <Route path="DeepCleaning" element={<DeepCleaning />}></Route>
-        {/* <Route path="/DeepCleaning" element={<DeepCleaning />}></Route>
-        <Route path="/DeepCleaning" element={<DeepCleaning />}></Route>
-        <Route path="/DeepCleaning" element={<DeepCleaning />}></Route> */}
-      </Routes>
-    </div>
-  );
-}
+// //   return element;
+// // };
+// function App() {
+//   return (
+//     <div className="root">
+//       <Routes>
+//         <Route path="/" element={<Home />}></Route>
+//         <Route path="/Home" element={<Home />}></Route>
+//         <Route path="./DeepCleaning" element={<DeepCleaning />}></Route>
+//         <Route path="/DeepCleaning" element={<DeepCleaning />}></Route>
+//         {/* <Route path="/DeepCleaning" element={<DeepCleaning />}></Route>
+//         <Route path="/DeepCleaning" element={<DeepCleaning />}></Route>
+//         <Route path="/DeepCleaning" element={<DeepCleaning />}></Route> */}
+//       </Routes>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,47 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import DeepCleaning from "./pages/Deepcleaning";
-
-import App from "./App";
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AboutUs from "./pages/Aboutus";
+import Home from "./pages/Home/index";
+import DeepCleaning from "./pages/Deepcleaning/index";
 import "./styles/tailwind.css";
 import "./styles/index.css";
 import "./styles/font.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    path: "/DeepCleaning",
-    element: <DeepCleaning />,
-  },
-]);
+function App() {
+  return (
+    <div>
+      <Routes>
+        {/* If the current URL is /about, this route is rendered
+        while the rest are ignored */}
+        {/* <Route path="/about">
+          <AboutUs />
+        </Route> */}
+        <Route exact path="/AboutUs" element={<AboutUs />} />
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+        {/* Note how these two routes are ordered. The more specific
+        path="/contact/:id" comes before path="/contact" so that
+        route will render when viewing an individual contact */}
+
+        <Route exact path="/" element={<Home />} />
+
+        {/* If none of the previous routes render anything,
+        this route acts as a fallback.
+
+        Important: A route with path="/" will *always* match
+        the URL because all URLs begin with a /. So that's
+        why we put this one last of all */}
+        {/* <Route path="/">
+          <Home />
+        </Route> */}
+      </Routes>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
 );
