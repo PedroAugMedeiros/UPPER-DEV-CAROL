4325827d4f779660b0b8ed62420238b3e71ce82c
PedroAugMedeiros <pedroaugmed7@gmail.com>
2024-10-23 19:20:47 -0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,33 @@
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+
+// root.render(<App />);
+import Home from "./pages/Home";
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/tailwind.css";
 import "./styles/index.css";
 import "./styles/font.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/hello",
+    element: <div>Hello</div>,
+  },
+]);
 
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
